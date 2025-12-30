ea8b67b2fd291d2723960de76994fb84f588fdbf
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2025-12-30 20:20:44 +0000

    adds custom color palette

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="h-screen w-full">
     <!-- Header -->
-    <div class="w-full p-16 flex flex-row-reverse items-center gap-16 font-exclamation">
+    <div class="w-full p-16 flex flex-row-reverse items-center gap-16 font-exclamation text-darkest">
       <div>
         My Account
       </div>
