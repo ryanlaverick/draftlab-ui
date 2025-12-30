2a940ae82394e34d9a8cc893f41c56f0e5ad6c82
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2025-12-30 20:55:17 +0000

    adds base spacing to entire page

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-  <div class="h-screen w-full">
+  <div class="h-screen w-full p-16">
     <!-- Header -->
-    <div class="w-full p-16 flex justify-between items-center gap-16 font-exclamation text-darkest">
+    <div class="w-full flex justify-between items-center gap-16 font-exclamation text-darkest mb-16">
       <div class="flex items-center gap-4 duration-300 hover:text-light">
         <Icon icon="uil:flask" class="size-8" />
         <h1 class="cursor-pointer">DraftLab</h1>
