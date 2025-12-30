d46b146ae6035c1f6db02bb279a2569f804c163e
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2025-12-30 23:39:00 +0000

    update team display

--- src/modules/simulator/components/Team.vue
+++ src/modules/simulator/components/Team.vue
@@ -3,16 +3,48 @@ defineProps({
   team: {
     type: Object,
     required: true
+  },
+  selected: {
+    type: Boolean,
+    required: true
   }
 })
+
+const emits = defineEmits(['clickTeam'])
 </script>
 
 <template>
-  <div class="border border-darkest rounded-md p-4 flex items-center gap-4">
-    <div class="w-full flex items-center justify-between gap-4">
-      <p class="uppercase font-semibold">{{ team.name }}</p>
+  <div
+    @click="emits('clickTeam', team)"
+    class="relative overflow-hidden
+         border-2 border-gray-200 rounded-md p-4
+         flex items-center gap-4 cursor-pointer
+         transition-colors duration-300
+         before:absolute before:inset-0
+         before:bg-light
+         before:origin-left
+         before:scale-x-0
+         before:transition-transform before:duration-300
+         before:[clip-path:polygon(0_0,75%_0,72%_100%,0_100%)]"
+    :class="{
+    'border-light before:scale-x-100': selected,
+    'hover:border-light': !selected
+  }"
+  >
+    <!-- Content above the bar -->
+    <div class="relative z-10 w-full flex items-center justify-between gap-4">
+      <p
+        class="uppercase font-semibold transition-colors duration-300"
+        :class="{ 'text-white': selected }"
+      >
+        {{ team.name }}
+      </p>
 
-      <img :src="team.image" :alt="'Team Logo for ' + team.name" class="size-10" />
+      <img
+        :src="team.image"
+        :alt="'Team Logo for ' + team.name"
+        class="size-10"
+      />
     </div>
   </div>
 </template>
--- src/modules/simulator/views/MockDraftSimulator.vue
+++ src/modules/simulator/views/MockDraftSimulator.vue
@@ -1,14 +1,40 @@
 <script setup>
 import useTeams from '@/modules/teams/useTeams.js'
 import Team from '@/modules/simulator/components/Team.vue'
+import { ref } from 'vue'
 
 const teams = useTeams
+
+const selectedTeams = ref([])
+
+const selectTeam = (team) => {
+  const index = selectedTeams.value.indexOf(team)
+
+  // Team has already been selected
+  if (index !== -1) {
+    selectedTeams.value.splice(index, 1)
+  } else {
+    selectedTeams.value.push(team)
+  }
+}
 </script>
 
 <template>
-  <p>Mock Draft Simulator</p>
+  <div class="w-full grid grid-cols-3 border-2 border-dark rounded-md">
+    <div class="bg-dark">
+      <div v-for="team in selectedTeams" :key="team.shortName" class="text-white">
+        {{ team.name }}
+      </div>
+    </div>
 
-  <div class="grid lg:grid-cols-4 sm:grid-cols-2 gap-4">
-    <team :key="team.shortName" v-for="team in teams" :team="team" />
+    <div class="p-4 col-span-2 flex flex-col gap-4">
+      <div class="flex flex-row-reverse">
+        Controls
+      </div>
+
+      <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4">
+        <team :key="team.shortName" v-for="team in teams" :team="team" :selected="selectedTeams.indexOf(team) !== -1" @click-team="selectTeam" />
+      </div>
+    </div>
   </div>
 </template>
