340daeb82cfb6372663afb1c79fb7682a7c6d776
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2025-12-31 23:18:53 +0000

    add hover underline styling to button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -4,7 +4,7 @@
   <button
     class="relative px-4 py-2 transform -skew-x-12 bg-dark rounded-md hover:bg-light duration-300 text-white cursor-pointer"
   >
-    <span class="inline-block transform skew-x-12 uppercase text-sm">
+    <span class="inline-block transform skew-x-12 uppercase text-sm hover:underline">
       <slot />
     </span>
   </button>
