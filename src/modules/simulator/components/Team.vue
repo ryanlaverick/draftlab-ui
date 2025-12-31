1d6283a1bfb2652389f11f2a8869f1b8748ee02e
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2025-12-31 23:12:12 +0000

    add alternate team color as border for team picker

--- src/modules/simulator/components/Team.vue
+++ src/modules/simulator/components/Team.vue
@@ -48,16 +48,59 @@ const backgroundColors = {
   'Tennessee Titans': 'before:bg-titans',
   'Washington Commanders': 'before:bg-commanders',
 }
+
+const borderColors = {
+  'Arizona Cardinals': 'border-cardinals-alt',
+  'Atlanta Falcons': 'border-falcons-alt',
+  'Baltimore Ravens': 'border-ravens-alt',
+  'Buffalo Bills': 'border-bills-alt',
+  'Carolina Panthers': 'border-panthers-alt',
+  'Chicago Bears': 'border-bears-alt',
+  'Cincinnati Bengals': 'border-bengals-alt',
+  'Cleveland Browns': 'border-browns-alt',
+  'Dallas Cowboys': 'border-cowboys-alt',
+  'Denver Broncos': 'border-broncos-alt',
+  'Detroit Lions': 'border-lions-alt',
+  'Green Bay Packers': 'border-packers-alt',
+  'Houston Texans': 'border-texans-alt',
+  'Indianapolis Colts': 'border-colts-alt',
+  'Jacksonville Jaguars': 'border-jaguars-alt',
+  'Kansas City Chiefs': 'border-chiefs-alt',
+  'Las Vegas Raiders': 'border-raiders-alt',
+  'Los Angeles Chargers': 'border-chargers-alt',
+  'Los Angeles Rams': 'border-rams-alt',
+  'Miami Dolphins': 'border-dolphins-alt',
+  'Minnesota Vikings': 'border-vikings-alt',
+  'New England Patriots': 'border-patriots-alt',
+  'New Orleans Saints': 'border-saints-alt',
+  'New York Giants': 'border-giants-alt',
+  'New York Jets': 'border-jets-alt',
+  'Philadelphia Eagles': 'border-eagles-alt',
+  'Pittsburgh Steelers': 'border-steelers-alt',
+  'San Francisco 49ers': 'border-49ers-alt',
+  'Seattle Seahawks': 'border-seahawks-alt',
+  'Tampa Bay Buccaneers': 'border-buccaneers-alt',
+  'Tennessee Titans': 'border-titans-alt',
+  'Washington Commanders': 'border-commanders-alt',
+}
+
+const computedClasses = computed(() => {
+  return [
+    backgroundColors[props.team.name],
+    props.selected ? borderColors[props.team.name] : '',
+    {
+      'before:scale-x-100': props.selected,
+      'border-gray-200 hover:border-light': !props.selected
+    }
+  ]
+})
 </script>
 
 <template>
   <div
     @click="emits('clickTeam', team)"
-    class="relative overflow-hidden border-2 border-gray-200 rounded-md p-4 flex items-center gap-4 cursor-pointer transition-colors duration-300 before:absolute before:inset-0 before:origin-left before:scale-x-0 before:transition-transform before:duration-300 before:[clip-path:polygon(0_0,75%_0,72%_100%,0_100%)]"
-    :class="[
-      backgroundColors[team.name],
-      { 'border-light before:scale-x-100': selected, 'hover:border-light': !selected },
-    ]"
+    class="relative overflow-hidden border-2  rounded-md p-4 flex items-center gap-4 cursor-pointer transition-colors duration-300 before:absolute before:inset-0 before:origin-left before:scale-x-0 before:transition-transform before:duration-300 before:[clip-path:polygon(0_0,75%_0,72%_100%,0_100%)]"
+    :class="computedClasses"
   >
     <!-- Content above the bar -->
     <div class="relative z-10 w-full flex items-center justify-between gap-4">
