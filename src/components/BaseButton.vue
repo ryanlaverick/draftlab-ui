c2e7b05a5f99b8e4fbebf0fb2ede7d88b8c6370c
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2025-12-31 23:18:35 +0000

    update text styling in base button component

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -4,7 +4,7 @@
   <button
     class="relative px-4 py-2 transform -skew-x-12 bg-dark rounded-md hover:bg-light duration-300 text-white cursor-pointer"
   >
-    <span class="inline-block transform skew-x-12">
+    <span class="inline-block transform skew-x-12 uppercase text-sm">
       <slot />
     </span>
   </button>
