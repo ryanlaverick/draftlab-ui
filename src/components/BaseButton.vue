9eef63048f2c0635d2b5bbfb45158ebd9c4afcd6
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2026-01-01 01:17:07 +0000

    change to use group hover for underlining text in buttons

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -2,9 +2,9 @@
 
 <template>
   <button
-    class="relative px-4 py-2 transform -skew-x-12 bg-dark rounded-md hover:bg-light duration-300 text-white cursor-pointer"
+    class="group relative px-4 py-2 transform -skew-x-12 bg-dark rounded-md hover:bg-light duration-300 text-white cursor-pointer"
   >
-    <span class="inline-block transform skew-x-12 uppercase text-sm hover:underline">
+    <span class="inline-block transform skew-x-12 uppercase text-sm group-hover:underline">
       <slot />
     </span>
   </button>
