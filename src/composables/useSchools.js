c05a5e4947029d9252107b4e66b0a5511d0aedeb
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2026-01-13 22:26:35 +0000

    create school composable, base logos

---
+++ src/composables/useSchools.js
@@ -0,0 +1,22 @@
+const schools = [
+  {
+    name: "Connecticut",
+    nickname: "Huskies",
+    shortName: "UCONN",
+    image: '/assets/school/connecticut-huskies-logo.png',
+  },
+  {
+    name: "Cal",
+    nickname: "Golden Bears",
+    shortName: "CAL",
+    image: '/assets/school/cal-golden-bears-logo.png',
+  },
+  {
+    name: "San Jose State",
+    nickname: "Spartans",
+    shortName: "S JOSE ST",
+    image: '/assets/school/san-jose-state-spartans-logo.png',
+  }
+]
+
+export default schools
