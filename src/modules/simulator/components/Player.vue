b976c7ff5913fc0f8485e2f1d3106466e0450db1
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2026-01-13 23:37:13 +0000

    add placeholder image when school logo cannot be found

--- src/modules/simulator/components/Player.vue
+++ src/modules/simulator/components/Player.vue
@@ -1,8 +1,9 @@
 <script setup>
 import SchoolLogo from '@/components/SchoolLogo.vue'
 import useSchools from '@/composables/useSchools.js'
-import { computed } from 'vue'
 import BaseButton from '@/components/BaseButton.vue'
+import { computed } from 'vue'
+import { Icon } from '@iconify/vue'
 
 const schools = useSchools
 
@@ -27,13 +28,14 @@ const getSchool = computed(() => {
   <div class="w-full border-y border-y-slate-200 bg-slate-50 hover:bg-slate-200 duration-300 p-4 flex items-center justify-between gap-8 snap-start cursor-pointer">
     <div class="flex items-center gap-4">
       <school-logo v-if="getSchool" :school="getSchool" class="size-10" />
+      <Icon v-else icon="mdi:image-remove-outline" class="size-10" />
 
-      <div class="flex flex-col text-md">
-        <span class="font-base">{{ player.player }}</span>
+      <div class="flex flex-col text-md text-dark/70">
+        <span class="font-bold">{{ player.player }}</span>
 
         <div class="flex items-center gap-2 text-sm">
-          <p class="font-bold">{{ player.position }}</p>
-          <p class="text-sm ">{{ getSchool?.name ?? 'Undefined' }}</p>
+          <p class="font-semibold">{{ player.position }}</p>
+          <p class="text-sm font-normal">{{ getSchool?.name ?? 'Undefined' }}</p>
         </div>
       </div>
 
