b641958d86e69264bc69b09904c1ecd2c9e2e168
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2026-02-03 21:14:15 +0000

    gracefully animate icon

--- src/components/DropdownLink.vue
+++ src/components/DropdownLink.vue
@@ -27,11 +27,43 @@ const isVisitingRoute = computed(() => {
   <router-link :to="{ name: route }">
     <dropdown-item :disabled="disabled">
       <div class="flex items-center gap-2">
-        <Icon v-if="isVisitingRoute" icon="bi:arrow-return-right" class="size-4" />
+        <Transition name="slide-expand" class="duration-300">
+          <Icon v-if="isVisitingRoute" icon="bi:arrow-return-right" class="size-4" />
+        </Transition>
+
         <slot />
       </div>
+
     </dropdown-item>
   </router-link>
 </template>
 
-<style scoped></style>
+<style scoped>
+.slide-expand-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-expand-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.slide-expand-enter-from {
+  opacity: 0;
+  transform: translateX(-10px);
+}
+
+.slide-expand-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.slide-expand-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.slide-expand-leave-to {
+  opacity: 0;
+  transform: translateX(-10px);
+}
+</style>
