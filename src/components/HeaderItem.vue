2f5358279b7f72aee2d634a5ca0d4f99051d8d76
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2026-02-03 12:36:20 +0000

    fix styling of header items

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -2,7 +2,7 @@
 defineProps({
   route: {
     type: String,
-    required: true,
+    required: false,
   },
 })
 </script>
@@ -11,10 +11,14 @@ defineProps({
   <div
     class="relative inline-block after:absolute after:left-0 after:-bottom-1 after:h-[6px] after:w-0 after:bg-light after:transition-all after:duration-300 hover:after:w-full after:[clip-path:polygon(0_0,100%_0,95%_100%,0_100%)] group cursor-pointer"
   >
-    <router-link :to="{ name: route }">
-      <div class="group-hover:duration-300 group-hover:text-light">
+    <router-link v-if="route" :to="{ name: route }">
+      <div class="group-hover:duration-300 group-hover:text-light font-exclamation">
         <slot />
       </div>
     </router-link>
+
+    <div v-else class="group-hover:duration-300 group-hover:text-light font-exclamation">
+      <slot />
+    </div>
   </div>
 </template>
