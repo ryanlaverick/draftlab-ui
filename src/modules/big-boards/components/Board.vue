9bbbe51e49a38f3db6864da44a39e0679349b955
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2026-02-03 22:55:20 +0000

    adds board display

---
+++ src/modules/big-boards/components/Board.vue
@@ -0,0 +1,64 @@
+<script setup>
+import dayjs from 'dayjs'
+import BaseButton from '@/components/BaseButton.vue'
+import { Icon } from '@iconify/vue'
+
+const props = defineProps({
+  board: {
+    type: Object,
+    required: true
+  }
+})
+
+const emits = defineEmits(['deleteBoard'])
+
+const deleteBoard = () => {
+  emits('deleteBoard', props.board.id)
+}
+</script>
+
+<template>
+  <div class="bg-base hover:bg-light duration-300 rounded-md h-64 w-full drop-shadow-lg drop-shadow-light cursor-pointer group transform -skew-x-12 overflow-hidden">
+    <div class="w-full h-5/12 bg-dark group-hover:bg-base duration-300 p-2 flex flex-col gap-2">
+      <div class="w-full flex items-center gap-2">
+        <div class="bg-base group-hover:bg-dark duration-300 rounded-full size-6"></div>
+        <div class="bg-base group-hover:bg-dark duration-300 rounded-md h-6 w-full"></div>
+      </div>
+
+      <div class="w-full flex items-center gap-2">
+        <div class="bg-base group-hover:bg-dark duration-300 rounded-full size-6"></div>
+        <div class="bg-base group-hover:bg-dark duration-300 rounded-md h-6 w-full"></div>
+      </div>
+
+      <div class="w-full flex items-center gap-2">
+        <div class="bg-base group-hover:bg-dark duration-300 rounded-full size-6"></div>
+        <div class="bg-base group-hover:bg-dark duration-300 rounded-md h-6 w-full"></div>
+      </div>
+    </div>
+
+    <div class="w-full h-7/12 flex flex-col gap-2 items-center text-white transform skew-x-12 relative group p-4">
+      <div class="absolute top-2 right-2 hidden group-hover:inline-block">
+        <base-button class="bg-red-500 hover:bg-red-700 !px-2 !py-1 flex justify-center" @click="deleteBoard">
+          <Icon icon="mdi:trash-outline" class="size-4 text-white" />
+        </base-button>
+      </div>
+
+      <div class="w-full flex justify-center text-center px-4">
+        <p class="font-exclamation truncate">{{ board.name }}</p>
+      </div>
+
+      <div class="flex flex-col justify-center items-center gap-1">
+        <span class="text-white/70 text-xs">Created at</span>
+        <span class="text-sm">{{ dayjs(board.created_at).format('dddd, MMMM D, YYYY h:mm A	') }}</span>
+      </div>
+
+      <div v-if="board.updated_at" class="flex flex-col justify-center items-center gap-1">
+        <span class="text-white/70 text-xs">Last Updated</span>
+        <span class="text-sm">{{ dayjs(board.updated_at).format('dddd, MMMM D, YYYY h:mm A	') }}</span>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/modules/big-boards/views/MyBoards.vue
+++ src/modules/big-boards/views/MyBoards.vue
@@ -1,7 +1,32 @@
-<script setup></script>
+<script setup>
+import Board from '@/modules/big-boards/components/Board.vue'
+
+const boards = [
+  {
+    id: 1,
+    name: 'Consensus Board',
+    created_at: '2026-01-01T07:30:00',
+    updated_at: null
+  },
+  {
+    id: 2,
+    name: 'New England Patriots Board',
+    created_at: '2025-12-17T11:51:13',
+    updated_at: '2026-02-03T22:35:29'
+  },
+  {
+    id: 3,
+    name: 'Test Board',
+    created_at: '2026-01-17T12:42:37',
+    updated_at: '2026-01-29T17:17:36'
+  }
+]
+</script>
 
 <template>
-  <p>My Boards</p>
+  <div class="w-full grid grid-cols-5 gap-8">
+    <board v-for="board in boards" :key="board.id" :board="board" />
+  </div>
 </template>
 
 <style scoped></style>
