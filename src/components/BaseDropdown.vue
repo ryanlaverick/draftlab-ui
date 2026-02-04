bb5dbc5bda8e80f88c5a85ab0f886a9ff3450230
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2026-02-04 00:08:36 +0000

    allow trigger mode for dropdown to be specified

--- src/components/BaseDropdown.vue
+++ src/components/BaseDropdown.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="relative inline-block text-left" @mouseenter="openMenu" @mouseleave="closeMenu">
+  <div v-if="trigger === 'hover'" class="relative inline-block text-left" @mouseenter="openMenu" @mouseleave="closeMenu">
+    <div>
+      <slot name="trigger" />
+    </div>
+
+    <!-- Dropdown -->
+    <Transition
+      enter-active-class="transition ease-out duration-200"
+      enter-from-class="opacity-0 scale-95 -translate-y-1"
+      enter-to-class="opacity-100 scale-100 translate-y-0"
+      leave-active-class="transition ease-in duration-150"
+      leave-from-class="opacity-100 scale-100 translate-y-0"
+      leave-to-class="opacity-0 scale-95 -translate-y-1"
+    >
+      <div
+        v-if="isOpen"
+        v-bind="$attrs"
+        class="absolute right-0 z-10 mt-2 w-64 origin-top-right rounded-md bg-base drop-shadow-lg drop-shadow-light text-white overflow-hidden transform -skew-x-12"
+      >
+        <slot />
+      </div>
+    </Transition>
+  </div>
+
+  <div v-else-if="trigger === 'click'" class="relative inline-block text-left" @click="openMenu" v-on-click-outside="closeMenu">
     <div>
       <slot name="trigger" />
     </div>
@@ -26,8 +50,20 @@
 
 <script setup>
 import { ref } from 'vue'
+import { vOnClickOutside } from '@vueuse/components'
 
 let isOpen = ref(false)
+
+defineProps({
+  trigger: {
+    type: String,
+    required: false,
+    default: 'hover',
+    validator(value) {
+      return ['hover', 'click'].includes(value)
+    },
+  },
+})
 
 const openMenu = () => {
   isOpen.value = true
